f7e841a4c045696588870636c382dda01ca89d9b
Dan Szafir <dszafir@gmail.com>
2011-12-08 18:53:26 -0600

    Removed some console logs

--- battle.js
+++ battle.js
@@ -8,17 +8,17 @@ function battle(Fleet1, Fleet2)
 	
 	if (List1.length > 0 && List2.length > 0)
 	{
-		console.log("List1 owner: "+ List1[0].owner);
-		console.log("List2 owner: "+ List2[0].owner);
+		//console.log("List1 owner: "+ List1[0].owner);
+		//console.log("List2 owner: "+ List2[0].owner);
 	
 		//For some reason, the program leaves this if loop without having reached the end
-		console.log("Battle started");
+		//console.log("Battle started");
 		//As you return from within this loop, this just goes on until done.
 		var battleGoing = true;
 		while(battleGoing)
 		//while( winner.length <= 0 )
 		{	
-			console.log("In while loop");
+			//console.log("In while loop");
 			//var attacker = List1[0];
 			//var target = List2[0];
 			//var attacker;
@@ -31,7 +31,7 @@ function battle(Fleet1, Fleet2)
 			attackList(List1, List2);
 			//If there are no defending surivors
 			if (List2.length <= 0){
-				console.log("BATTLE FINISHED. k: " + k);				
+				//console.log("BATTLE FINISHED. k: " + k);				
 				return List1;				
 			}
 			
@@ -39,7 +39,7 @@ function battle(Fleet1, Fleet2)
 			attackList(List2, List1);
 			//If there are no attacking survivors
 			if (List1.length <= 0){
-				console.log("BATTLE FINISHED. k: " + k);				
+				//console.log("BATTLE FINISHED. k: " + k);				
 				return List2;				
 			}
 			k++;
@@ -47,10 +47,10 @@ function battle(Fleet1, Fleet2)
 
 	}
 	else{
-		console.log("Returning empty winner. This is an error.");
+		//console.log("Returning empty winner. This is an error.");
 		//return winner;	
 	}
-	console.log("I'm returning nothing and ending. This should not be possible.");
+	//console.log("I'm returning nothing and ending. This should not be possible.");
 }
 
 // !!! This is not a nice way to do this. But let's see how it works for now.
@@ -89,7 +89,7 @@ function attackList(AttackerList, DefenderList){
 	//Remove casualties from array.
 	for (var i = DefenderList.length - 1; i > -1; i--){
 		if (!DefenderList[i].alive){
-			console.log("Someone died!");
+			//console.log("Someone died!");
 			DefenderList.splice(i,1);
 		}
 	}
